a66301282402718c0b98c0eaf8bb2d7f8d0c5e70
SkimJazz <jb.skimjazz@outlook.com>
2024-07-13 16:33:41 +1000

    feat: Add Landing page and Update dependencies
    - Updated several dependencies in package.json to their latest versions.
    - Added new CSS rules to `index.css` to enhance Landing page background img.
    - Added Components directory with initial setup of Logo and its components

---
+++ client/src/components/Logo.jsx
@@ -0,0 +1,7 @@
+import logo from '../assets/images/logo.svg';
+
+// Create the component
+const Logo = () => {
+    return <img src={logo} alt='tradesequip' className='logo' />;
+};
+export default Logo;
